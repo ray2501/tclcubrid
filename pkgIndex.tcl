26118ea148e76ea1f9f806726a5c2f5c0cf29c75
Danilo Chang <ray2501@gmail.com>
2023-12-16 21:36:00 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,8 +1,13 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded cubrid 0.9.6 \
-    [list load [file join $dir libcubrid0.9.6.so] cubrid]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded cubrid 0.9.6 \
+	    [list load [file join $dir libtcl9cubrid0.9.6.so] [string totitle cubrid]]
+} else {
+    package ifneeded cubrid 0.9.6 \
+	    [list load [file join $dir libcubrid0.9.6.so] [string totitle cubrid]]
+}
 
 package ifneeded tdbc::cubrid 0.9.6 \
     [list source [file join $dir tdbccubrid.tcl]]
